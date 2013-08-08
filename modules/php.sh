dd48a26ef7bdff8780e0d1fab126db2612e6e421
Michal Piotrowski <michal@eventhorizon.pl>
2013-08-08 19:23:16 +0200

    sic p13

--- modules/php.sh
+++ modules/php.sh
@@ -22,7 +22,7 @@ function configure_package()
 function install_package()
 {
     show_message "Installing php..."
-    yum install --assumeyes php php-bcmath php-cli php-common php-fpm php-gd php-gmp php-imap php-intl php-mbstring php-mcrypt php-mysqlnd php-opcache php-pdo php-pgsql php-process php-snmp php-xml php-pecl-imagick php-pecl-xdebug php-pecl-mongo php-pecl-memcache php-pecl-memcached php-phpunit-* >> /tmp/install.log 2>&1
+    yum install --assumeyes php php-bcmath php-cli php-common php-fpm php-gd php-gmp php-imap php-intl php-mbstring php-mcrypt php-mysqlnd php-opcache php-pdo php-pgsql php-process php-snmp php-xml php-pecl-imagick php-pecl-apcu php-pecl-xdebug php-pecl-mongo php-pecl-memcache php-pecl-memcached php-phpunit-* >> /tmp/install.log 2>&1
     show_result $?
 }
 

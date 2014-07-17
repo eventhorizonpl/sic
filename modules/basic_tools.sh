96b0a8375a95b7696fb932f6f6c5c1c993678807
Michal Piotrowski <michal@eventhorizon.pl>
2014-07-18 00:08:42 +0200

    rhel 7 support p8

--- modules/basic_tools.sh
+++ modules/basic_tools.sh
@@ -6,13 +6,13 @@ function install_package()
 {
     if [ $OS == "rhel" ]
     then
-        show_message "\tInstalling EPEL release package..."
-        rpm -ihv http://ftp.pbone.net/pub/fedora/epel/beta/7/x86_64/epel-release-7-0.2.noarch.rpm
+        show_message "Installing EPEL release package..."
+        rpm -ihv http://dl.fedoraproject.org/pub/epel/beta/7/x86_64/epel-release-7-0.2.noarch.rpm >> /tmp/install.log 2>&1
         show_result $?
     fi
 
     show_message "Installing basic tools..."
-    yum install --assumeyes acl bootchart bzip2 cmake cryptsetup deltarpm ecryptfs-utils fedup \
+    yum install --assumeyes acl bootchart bzip2 cmake cryptsetup deltarpm fedup \
 	gcc gcc-c++ git java libsqlite3x libsqlite3x-devel lm_sensors lucene make mc \
 	net-tools nodejs npm patch pcre-devel policycoreutils-python redhat-rpm-config \
 	screen subversion tar tigervnc-server unixODBC-devel unzip vim wget >> /tmp/install.log 2>&1

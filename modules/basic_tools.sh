e7cc7c2ccc7a035f77a99f036716c891906509d4
Michal Piotrowski <michal@eventhorizon.pl>
2014-07-25 15:09:25 +0200

    sic p29

--- modules/basic_tools.sh
+++ modules/basic_tools.sh
@@ -13,7 +13,7 @@ function install_package()
 
     show_message "Installing basic tools..."
     yum install --assumeyes acl bootchart bzip2 cmake cryptsetup deltarpm ecryptfs-utils fedup \
-	gcc gcc-c++ git java libsqlite3x libsqlite3x-devel lm_sensors lucene make mc \
+	gcc gcc-c++ git java libsqlite3x libsqlite3x-devel lm_sensors lsof lucene make mc \
 	net-tools nodejs npm patch pcre-devel policycoreutils-python redhat-rpm-config \
 	screen subversion tar tigervnc-server unixODBC-devel unzip vim wget >> /tmp/install.log 2>&1
     show_result $?

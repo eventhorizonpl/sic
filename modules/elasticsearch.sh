3be5c69108fcc9d4635e25dcfaa2cada9ea05d5f
Michal Piotrowski <michal@eventhorizon.pl>
2016-03-30 14:42:19 +0200

    update elastic search

--- modules/elasticsearch.sh
+++ modules/elasticsearch.sh
@@ -40,7 +40,7 @@ function install_package()
         show_result $?
     elif [ $OS == "rhel" ]
     then
-        rpm -ihv https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.1.noarch.rpm >> /tmp/install.log 2>&1
+        rpm -ihv https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/2.2.1/elasticsearch-2.2.1.rpm >> /tmp/install.log 2>&1
         show_result $?
     fi
 }
